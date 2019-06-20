d042b8129844703b5742badebc3c795a387eed24
Ryan Wilson <r@trusona.com>
2019-06-19 17:31:27 -0700

    Merged both webpack config files into one

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const packageJson = require('./package.json')
 
-module.exports = {
+module.exports = [{
   entry: './src/index.js',
   devServer: {
     contentBase: './dist'
@@ -23,4 +23,27 @@ module.exports = {
       use: 'babel-loader'
     }]
   }
-};
+}, {
+  entry: './src/index.js',
+  devServer: {
+    contentBase: './dist'
+  },
+  output: {
+    filename: `trucode-${packageJson.version}.js`,
+    path: path.resolve(__dirname, 'dist'),
+    library: 'Trusona',
+    libraryTarget: 'var',
+    libraryExport: 'default'
+  },
+  mode: 'production',
+  node: {
+    fs: 'empty'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: 'babel-loader'
+    }]
+  }
+}];
